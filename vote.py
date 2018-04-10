a257d620adea83fba9cdbb0a2b4180fced7f2fa7
dsoup <krdsoup@gmail.com>
2018-04-10 12:00:23 +0900

    params 0 을 address로 변경

--- vote.py
+++ vote.py
@@ -1,3 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright 2018 theloop, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""ICX SCORE Prototyping code to explain basic SCORE example, NOT OFFICIAL ICX CODE!!!!
+NO GUARANTEE FOR ANY RISK OR TROUBLE.
+"""
+
+
 import leveldb
 import json
 
@@ -238,7 +260,7 @@ class UserScore(ScoreBase):
 
         params = request['params']
         self.logd(params)
-        address = params['address']
+        address = params[0]
         self.logd(address)
 
         if not check_address(address):
