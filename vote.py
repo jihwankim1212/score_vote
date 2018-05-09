a635b60cbe2cba99cc88e024115de13358d04618
dsoup <krdsoup@gmail.com>
2018-05-09 15:23:08 +0900

    params 0 을 address로 변경

--- vote.py
+++ vote.py
@@ -281,8 +281,8 @@ class UserScore(ScoreBase):
             selectAddress = itemAddress[itemIdx]
             self.logd('__invoke_voteTx() voteAddress : ' + createAddress + '_' + str(itemIdx))
             set_balance_str(self.__db, createAddress + '_' + str(itemIdx), selectAddress)
-            set_balance(self.__db, 'item_' + str(itemAddress) + '_cnt',
-                        get_balance(self.__db, 'item_' + str(itemAddress) + '_cnt') + 1)
+            set_balance(self.__db, 'item_' + selectAddress + '_cnt',
+                        get_balance(self.__db, 'item_' + selectAddress + '_cnt') + 1)
             self.logd('__invoke_voteTx() getBalance key : ' + 'item_' + str(itemAddress) + '_cnt')
             self.logd('__invoke_voteTx() getBalance : ' + str(get_balance(self.__db, 'item_' + str(itemAddress) + '_cnt')))
             itemIdx = itemIdx + 1
